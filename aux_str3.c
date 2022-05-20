f0568049db5c5ec3ffd3180ba60b61ce2ba3229c
Decypher1 <mumekwe2@gmail.com>
2022-05-19 18:51:45 -0700

    Edited file

--- aux_str3.c
+++ aux_str3.c
@@ -1,70 +1,30 @@
 #include "main.h"
 
 /**
- * error_env - error message for env in get_env.
- * @datash: data relevant (counter, arguments)
- * Return: error message.
+ * rev_string - reverses a string.
+ * @s: input string.
+ * Return: no return.
  */
-char *error_env(data_shell *datash)
+void rev_string(char *s)
 {
-	int length;
-	char *error;
-	char *ver_str;
-	char *msg;
+	int count = 0, i, j;
+	char *str, temp;
 
-	ver_str = aux_itoa(datash->counter);
-	msg = ": Unable to add/remove from environment\n";
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + _strlen(msg) + 4;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	while (count >= 0)
 	{
-		free(error);
-		free(ver_str);
-		return (NULL);
+		if (s[count] == '\0')
+			break;
+		count++;
 	}
+	str = s;
 
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, msg);
-	_strcat(error, "\0");
-	free(ver_str);
-
-	return (error);
-}
-
-/**
- * error_path_126 - error message for path and failure denied permission.
- * @datash: data relevant (counter, arguments).
- *
- * Return: The error string.
- */
-char *error_path_126(data_shell *datash)
-{
-	int length;
-	char *ver_str;
-	char *error;
-
-	ver_str = aux_itoa(datash->counter);
-	length = _strlen(datash->av[0]) + _strlen(ver_str);
-	length += _strlen(datash->args[0]) + 24;
-	error = malloc(sizeof(char) * (length + 1));
-	if (error == 0)
+	for (i = 0; i < (count - 1); i++)
 	{
-		free(error);
-		free(ver_str);
-		return (NULL);
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
 	}
-	_strcpy(error, datash->av[0]);
-	_strcat(error, ": ");
-	_strcat(error, ver_str);
-	_strcat(error, ": ");
-	_strcat(error, datash->args[0]);
-	_strcat(error, ": Permission denied\n");
-	_strcat(error, "\0");
-	free(ver_str);
-	return (error);
 }
